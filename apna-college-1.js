16c1ffdd14238fad81ddab456dde6ad47c452e5f
Saif Ali Khan <81072571+Saif843@users.noreply.github.com>
2024-10-18 02:21:38 +0500

    Update apna-college-1.js

--- apna-college-1.js
+++ apna-college-1.js
@@ -1,11 +1,4 @@
 
-//connstant value generally comes in capital letters
-// all about data-types, operators
-
-
-// ------------------------------------
-
-// Lecture - 3
 
 <script type="text/javascript">  
  alert("Hello Javatpoint");  
