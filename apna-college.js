076b5a90479ce2542841b9645b8bd7b57b0ff82c
Saif Ali Khan <81072571+Saif843@users.noreply.github.com>
2024-10-18 02:21:22 +0500

    Update apna-college.js

--- apna-college.js
+++ apna-college.js
@@ -1,28 +1,8 @@
-//process of solving problem
-//Understand the problem what it say
-//input clarity
-//solution
-
-//converting it into code
-
-//action, process other than input and output come in the rectangle box
-//decision component gives me yes or no
-
-//Pseudo code is general logic of solution
-//logic and code middle thing lies in the pesudocode
-//Think about solution when understanding the problem
-
-//Dry run means example lekr apne solution ko analyze krna
-//2 is a special even prime number
-//Pseudo.png refrenced
-//when exit occur execution stops
-
-//Dsa is logic not math
 
 
 
-//finding Sum
-
-let number;
+<script type="text/javascript">  
+ alert("Hello Javatpoint");  
+</script>  
 
 
